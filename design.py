42f15d4cce43129386c21afa93d9e4be28a88da5
Rishabh Mishra <rishabhmishra328@gmail.com>
2018-01-04 22:37:01 +0530

    Initial  commit

---
+++ design.py
@@ -0,0 +1,6 @@
+import numpy as np
+from keras.applications import vgg16
+from keras import backend as K
+from keras.preprocessing.image import load_img, img_to_array
+# import image_processing as img
+from IPython.display import Image
